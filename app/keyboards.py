0242850577230f27d87b1e23394bc701250c6c3d
pavilk <pkarenginp@gmail.com>
2025-05-16 23:16:05 +0500

    Add registration

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,4 +1,34 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                                InlineKeyboardMarkup, InlineKeyboardButton)
 
+from aiogram.utils.keyboard import  ReplyKeyboardBuilder, InlineKeyboardBuilder
 
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Зарегистрироваться', callback_data='registration')]
+])
+
+main_reply = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Зарегистрироваться")],
+        [KeyboardButton(text="What?")]
+    ],
+    resize_keyboard=True
+)
+
+reply_test = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Пися")],
+        [KeyboardButton(text="What?")]
+    ],
+    resize_keyboard=True
+)
+
+settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='ХУЙЧИК', url='https://unit-ekb.ru/')]])
+
+manus = ['Platonn228', 'IVANUS', 'STEPA', 'ZALUPA']
+async def inline_manus():
+    keyboard = InlineKeyboardBuilder()
+    for man in manus:
+        keyboard.add(InlineKeyboardButton(text=man, url='https://github.com/Platonn3'))
+    return keyboard.adjust(1).as_markup()
 
