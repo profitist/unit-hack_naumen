cb4681a20a0058fb64dc0d3033bd22605ffe999b
pavilk <pkarenginp@gmail.com>
2025-05-17 22:25:06 +0500

    admin keyboard

--- app/handlers/user_handlers.py
+++ app/handlers/user_handlers.py
@@ -46,7 +46,7 @@ async def cmd_start(message: Message, command: CommandObject):
         is_admin = await rq.is_admin(message.from_user.id)
         if is_admin:
             await message.answer(tu.send_start_admin_user_message(message),
-                                 reply_markup=kb.main_reply)
+                                 reply_markup=kb.admin)
         else:
             await message.answer(tu.send_start_common_user_message(message),
                                  reply_markup=kb.main_reply)
