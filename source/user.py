ac7b1b951656e4779be83082960a02839cb76108
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2025-05-17 12:44:45 +0500

    add simple user

---
+++ source/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self):
+        self.user_id = ''
+        self.tg_id = ''
+        self.username = ''
+        self.first_name = ''
+        self.last_name = ''
+        self.phone_number = ''
+        self.is_admin = False
